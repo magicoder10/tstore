d094c31c7fbd05707d37f221be970d3e95cd6274
Magic Coder <magicoder20@gmail.com>
2022-03-23 00:17:40 -0700

    Clean reliable IDGen (#4)

--- idgen/idgen.go
+++ idgen/idgen.go
@@ -1,17 +1,24 @@
 package idgen
 
+import (
+	"encoding/json"
+
+	"tstore/storage"
+)
+
 type IDGen struct {
-	storage    Storage
-	bufferSize int
-	nextID     uint64
-	nextIDs    chan uint64
-	remain     uint64
+	storagePath string
+	rawMap      storage.RawMap
+	bufferSize  int
+	nextID      uint64
+	nextIDs     chan uint64
+	remain      uint64
 }
 
 func (i IDGen) NextID() (uint64, error) {
 	if len(i.nextIDs) == 0 {
 		nextID := i.nextID + uint64(i.bufferSize)
-		err := i.storage.writeNextID(nextID)
+		err := i.writeNextID(nextID)
 		if err != nil {
 			return 0, err
 		}
@@ -25,16 +32,45 @@ func (i IDGen) NextID() (uint64, error) {
 	return <-i.nextIDs, nil
 }
 
-func New(storage Storage, bufferSize int) (IDGen, error) {
-	nextID, err := storage.readNextID()
+func (i IDGen) writeNextID(nextID uint64) error {
+	buf, err := json.Marshal(nextID)
+	if err != nil {
+		return err
+	}
+
+	return i.rawMap.Set(i.storagePath, buf)
+}
+
+func (i IDGen) readNextID() (uint64, error) {
+	return readNextID(i.storagePath, i.rawMap)
+}
+
+func New(storagePath string, rawMap storage.RawMap, bufferSize int) (IDGen, error) {
+	nextID, err := readNextID(storagePath, rawMap)
 	if err != nil {
 		return IDGen{}, err
 	}
 
 	return IDGen{
-		storage:    storage,
-		bufferSize: bufferSize,
-		nextID:     nextID,
-		nextIDs:    make(chan uint64, bufferSize),
+		storagePath: storagePath,
+		rawMap:      rawMap,
+		bufferSize:  bufferSize,
+		nextID:      nextID,
+		nextIDs:     make(chan uint64, bufferSize),
 	}, nil
+}
+
+func readNextID(path string, rawMap storage.RawMap) (uint64, error) {
+	buf, err := rawMap.Get(path)
+	if err != nil {
+		return 0, err
+	}
+
+	var nextID uint64
+	err = json.Unmarshal(buf, &nextID)
+	if err != nil {
+		return 0, err
+	}
+
+	return nextID, nil
 }
